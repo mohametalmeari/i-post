cc2e379d6ad26d55606e558fca615c1fff9b4b03
mohametalmeari <mohamet.almeari@gmail.com>
2023-11-30 22:08:12 +0300

    Add uniqueness validation to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :username, presence: true,
-                       format: { with: /\A[a-zA-Z][a-zA-Z0-9]*\z/, message: 'can only contain letters and numbers' }
+                       format: { with: /\A[a-zA-Z][a-zA-Z0-9]*\z/, message: 'can only contain letters and numbers' },
+                       uniqueness: { case_sensitive: false, message: 'has already been taken' }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
