e3c7bcb363abd7f4967b0eae09386b16afc4f153
mohametalmeari <mohamet.almeari@gmail.com>
2023-11-30 17:02:41 +0300

    Add username to views forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :username, presence: true,
                        format: { with: /\A[a-zA-Z][a-zA-Z0-9]*\z/, message: 'can only contain letters and numbers' }
-  validates :role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
