2c7e43d27e45d1bacfcfd69237a45ddcce802407
mohametalmeari <mohamet.almeari@gmail.com>
2023-12-03 21:34:09 +0300

    Implement comment reply and reply like methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,45 @@ class CommentsController < ApplicationController
   def show
     @comment = Comment.find_by(post_id: params[:post_id], id: params[:id])
     @comment_like = CommentLike.new
+    @comment_reply = CommentReply.new
+    @comment_replies = CommentReply.where(comment: @comment)
+  end
+
+  def reply
+    comment_reply = CommentReply.new(reply_params)
+    comment_reply.user = current_user
+    flash[:notice] = if comment_reply.save
+                       'You replied on the comment'
+                     else
+                       comment_reply.errors.full_messages.join(', ')
+                     end
+    redirect_to request.referrer
+  end
+
+  def like_reply
+    like = ReplyLike.find_by(comment_reply: reply_like_param[:comment_reply_id], user: current_user)
+    if like
+      like.destroy
+      flash[:notice] = 'You unliked the comment'
+    else
+      reply_like = ReplyLike.new(reply_like_param)
+      reply_like.user = current_user
+      flash[:notice] = if reply_like.save
+                         'You liked the comment'
+                       else
+                         reply_like.errors.full_messages.join(', ')
+                       end
+    end
+    redirect_to request.referrer
+  end
+
+  private
+
+  def reply_params
+    params.require(:comment_reply).permit(:comment_id, :content)
+  end
+
+  def reply_like_param
+    params.require(:reply_like).permit(:comment_reply_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   end
   post 'like', to: 'posts#like', as: 'like'
   post 'comment_like', to: 'posts#like_comment', as: 'comment_like'
+  post 'reply_like', to: 'comments#like_reply', as: 'reply_like'
   post 'comment', to: 'posts#comment', as: 'comment'
+  post 'reply', to: 'comments#reply', as: 'comment_reply'
   devise_for :users
   root "home#index"
   get 'about', to: 'home#about'
