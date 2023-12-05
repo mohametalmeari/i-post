f31d33b2f97721e9969751918cf305803375ad86
mohametalmeari <mohamet.almeari@gmail.com>
2023-12-05 03:42:08 +0300

    Create Recent section on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @all_posts = Post.where('public = ? OR user_id = ? OR ?', true, current_user.id, current_user.role == 'admin')
+    # @posts = @all_posts.where(public: true).order(created_at: :desc).limit(3)
+    @posts = @all_posts.order(created_at: :desc).limit(3)
+  end
 
   def about; end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
   has_many :post_likes, foreign_key: 'post_id', dependent: :destroy
   has_many :comments, foreign_key: 'post_id', dependent: :destroy
   validates :title, presence: true
+  validates :content, presence: true
 end
 return
