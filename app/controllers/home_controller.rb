b3ff029b1ff4b08e36674e544bc65c5d1342cae4
mohametalmeari <mohamet.almeari@gmail.com>
2023-12-05 05:28:54 +0300

    Style Posts page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @all_posts = Post.where('public = ? OR user_id = ? OR ?', true, current_user.id, current_user.role == 'admin')
     # @posts = @all_posts.where(public: true).order(created_at: :desc).limit(3)
-    @posts = @all_posts.order(created_at: :desc).limit(3)
+    @posts = @all_posts.order(created_at: :desc).limit(4)
   end
 
   def about; end
