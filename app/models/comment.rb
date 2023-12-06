c63f392b8880bc978a121a566a8df523bceed31a
mohametalmeari <mohamet.almeari@gmail.com>
2023-12-07 01:53:42 +0300

    Style comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,15 @@ class Comment < ApplicationRecord
   has_many :comment_likes, foreign_key: 'comment_id', dependent: :destroy
   has_many :comment_replies, foreign_key: 'comment_id', dependent: :destroy
   validates :content, presence: true
+  def self.count_comments(post)
+    count = where(post: post).count
+    if count == 1
+      "#{count} Comment"
+    elsif count == 0
+      "No Comments"
+    else
+      "#{count} Comments"
+    end
+  end
 end
 return
