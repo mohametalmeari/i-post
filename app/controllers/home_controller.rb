7e61e4f5ed831a1c27095a12974a2e6622eb2d2a
mohametalmeari <mohamet.almeari@gmail.com>
2023-12-07 14:55:52 +0300

    Allow not signed in users to view posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @all_posts = Post.where('public = ? OR user_id = ? OR ?', true, current_user.id, current_user.role == 'admin')
-    # @posts = @all_posts.where(public: true).order(created_at: :desc).limit(3)
+    @all_posts = if user_signed_in?
+                   Post.where('public = ? OR user_id = ? OR ?', true, current_user.id, current_user.role == 'admin')
+                 else
+                   Post.where(public: true)
+                 end
     @posts = @all_posts.order(created_at: :desc).limit(4)
   end
 
