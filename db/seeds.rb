850e2691e0e514df081affb0f74e09c9fd03940c
mohametalmeari <mohamet.almeari@gmail.com>
2023-12-07 17:37:44 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 admin = User.find_by( role: 'admin' )
-User.create(name: 'Admin',username: 'admin', email: 'admin@mo-dev.site', password: '753951', role: 'admin') if admin
+User.create(name: 'Admin',username: 'admin', email: 'admin@mo-dev.site', password: '753951', role: 'admin') unless admin
