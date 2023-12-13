b02c1af31dba845667c7ebccd59d485e21ca8c61
mohametalmeari <mohamet.almeari@gmail.com>
2023-12-13 15:20:53 +0300

    Create rspec tests for comment

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   user = User.new(email: 'user@email.com', password: '753951', username: 'user', name: 'user')
+
   it 'title should be present' do
-    post = Post.new(content: 'Bla bla bla ...', user: user)
+    post = Post.new(content: 'Bla bla bla ...', user:)
     expect(post).to_not be_valid
   end
 
   it 'content should be present' do
-    post = Post.new(title: 'New Post', user: user)
+    post = Post.new(title: 'New Post', user:)
     expect(post).to_not be_valid
   end
 
@@ -16,5 +17,4 @@ RSpec.describe Post, type: :model do
     post = Post.new(title: 'New Post', content: 'Bla bla bla ...')
     expect(post).to_not be_valid
   end
-
 end
